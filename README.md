42d37ddd7fc2d601a8b4a5b33da39ce01afd4ae6
biancaregulski <biancaregulski@yahoo.com>
2023-10-27 12:46:25 -0400

    update argument input, details for methods

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # DogParkFinder
 Find closest dog parks from 2 different origin points
 
+Sample call: 
+GET /park
 
-#### OPTIONS for finding dog park
+arguments:
+address1 (string): description of 1st address in natural language
+address2 (string):description of 2nd address in natural language
+transportation (string, optional): transit method ('driving' (default), 'walking', 'biking', 'transit')
+
+On local dev, to find closest dog park between Boston Common and Porter Square in Cambridge by walking:
+http://127.0.0.1:5000/park?address1=4%20Charles%20Street.%20Boston,%20MA%2002116&address2=Porter Square, Cambridge MA&transportation=walking
+
+# Future Directions:
+- instead of choosing default closest dog park, compare ~5 by their distance to each given address
+
+#### Options for finding dog park
 * = origins
 o = destinations (dog parks)
 
-option 1: haversine distance for the shape of the earth
+option 1: calculate distance between 2 points (e.g. haversine distance for the shape of the earth)
 pro: not reliant on api beyond getting lat/long coordinates
 con: doesn't take into account bodies of water, traffic, impassible areas, etc.
 --------------------------------------
--- api.py
+++ api.py
@@ -17,26 +17,21 @@ def healthcheck():
 @app.route('/park', methods=['GET'])
 def get_optimal_park():
 	args = request.args
-	for required in ['origin', 'destination']:
+	for required in ['address1', 'address2']:
 		# check for blank or empty params
 		if not args.get(required):
 			return jsonify(status="missing_input", message="Missing param: '{}'".format(required)), 422
 
-	origin = args.get('origin')
-	destination = args.get('destination')
+	origin = args.get('address1')
+	destination = args.get('address2')
 	transportation = args.get('transportation')
-	use_multiple_routes = args.get('multiple_routes')
 
 	# transportation is optional but must be in enum if present
 	if transportation and transportation not in Transportation.get_values():
 		return jsonify(status="invalid_input", message="Invalid transportation param: '{}'".format(transportation)), 400
 	
-	try:
-		midpoint = get_midpoint_for_addresses(gmaps, origin, destination, use_multiple_routes)
-		dog_parks = get_dog_parks_for_location(gmaps, midpoint)
-	except Exception as e:
-		return jsonify(status="server_error", message=str(e)), 400
-		# TODO: narrow exceptions
+	midpoint = get_midpoint_for_addresses(gmaps, origin, destination)
+	dog_parks = get_dog_parks_for_location(gmaps, midpoint)
 
 	data = {
 		'status': 'success',
